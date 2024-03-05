5df16785f5da232ada5785784724763c8987ef07
klypo.d <klypo.dmitrii@gmail.com>
2024-03-05 12:11:41 +0200

    grammy added

--- src/components/QrScanner.vue
+++ src/components/QrScanner.vue
@@ -1,17 +1,25 @@
 <template>
+
 	<div class="scanner">
 		<qrcode-stream @detect="onDetect"></qrcode-stream>
 	</div>
 	<p v-if="content">{{ content }}</p>
+	<p>-----------------------------------------</p>
+	<p>{{WebApp}}</p>
 </template>
 
 <script setup lang="ts">
 import { QrcodeStream } from 'vue-qrcode-reader'
-import {ref} from "vue";
+import { WebApp } from "@grammyjs/web-app";
+import {onMounted, ref} from "vue";
 const onDetect = (ctx: object) => {
 	content.value = ctx
 }
 const content = ref<object>({})
+const webAppData = ref()
+onMounted(() => {
+	WebApp.ready()
+})
 </script>
 
 <style scoped>
