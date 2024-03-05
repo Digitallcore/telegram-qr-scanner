b6e255ede623d296464f2025bec45db7b781b394
klypo.d <klypo.dmitrii@gmail.com>
2024-03-05 13:19:11 +0200

    Telegram scanner added

--- src/components/QrScanner.vue
+++ src/components/QrScanner.vue
@@ -1,4 +1,5 @@
 <template>
+	<button class="qr-button" @click="showScannerPopup">Open again</button>
 	<p>{{context}}</p>
 </template>
 
@@ -6,14 +7,17 @@
 import {onMounted, ref} from "vue";
 import { WebApp } from "@grammyjs/web-app";
 const context = ref<string>('')
+const showScannerPopup = () => {
+	WebApp.showScanQrPopup({text: 'Scan your QR code! :)'}, async (data: string) => {
+		context.value = data
+	})
+}
 
 onMounted(() => {
 	WebApp.ready()
 	// main button settings
+	showScannerPopup()
 	WebApp.expand()
-	WebApp.showScanQrPopup({text: 'Scan your QR code! :)'}, async (data: string) => {
-		context.value = data
-	})
 	WebApp.MainButton.text = 'Close QR Scanner'
 	WebApp.MainButton.isVisible = true
 	WebApp.MainButton.onClick(() => WebApp.close())
